aac8b0e80c29926196e66ce9168e6adda98fbd73
michael shahin <shahinmg3@gmail.com>
2025-10-14 16:29:21 -0500

    initial commit for sphere volume calculation

---
+++ sphere_volume.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: laserglaciers
+"""
+
+import math
+
+def sphere_volume(radius):
+  
+  if radius < 0:
+    raise ValueError("Radius cannot be negative.")
+    
+  volume = (4/3) * math.pi * (radius ** 3)
+  
+  return volume
